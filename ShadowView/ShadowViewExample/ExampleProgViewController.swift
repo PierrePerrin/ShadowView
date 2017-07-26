02093741e9e2cc25ffdd78f55902541404b28339
Pierre <pierreperrin@outlook.com>
2017-07-26 17:45:47 -0400

    Update ressources

---
+++ ShadowView/ShadowViewExample/ExampleProgViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ExampleProgViewController.swift
+//  ShadowView
+//
+//  Created by Pierre Perrin on 26/07/2017.
+//  Copyright © 2017 Pierreperrin. All rights reserved.
+//
+
+import UIKit
+
+class ExampleProgViewController: UIViewController {
+
+    let exampleShadowContainerView = ShadowView()
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "sample.jpg"))
+    
+    override func loadView() {
+        super.loadView()
+        
+        exampleShadowContainerView.frame = self.view.bounds
+        exampleShadowContainerView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        exampleShadowContainerView.shadowOffset = CGSize(width: 0, height: 10)
+        exampleShadowContainerView.shadowRadius = 20
+        
+        self.view.addSubview(exampleShadowContainerView)
+        self.exampleShadowContainerView.addSubview(imageView)
+        imageView.center = exampleShadowContainerView.center
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.frame.size = CGSize(width: 200, height: 200)
+        imageView.center = exampleShadowContainerView.center
+        self.exampleShadowContainerView.updateShadow()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ShadowView/ShadowViewExample/ViewController.swift
+++ ShadowView/ShadowViewExample/ViewController.swift
@@ -12,39 +12,45 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var exampleView: ShadowView!
     @IBOutlet weak var imageviEW: UIImageView!
+    @IBOutlet weak var otherImage: UIImageView!
     
     let image1 = #imageLiteral(resourceName: "sample.jpg")
     let image2 = #imageLiteral(resourceName: "home2.jpeg")
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        
     }
     
     @IBAction func blurSlider(_ sender: UISlider) {
         
         self.exampleView.shadowRadius = CGFloat(sender.value * 50)
+        self.otherImage.shadowRadius = CGFloat(sender.value * 50)
     }
     
     @IBAction func shadowSize(_ sender: UISlider) {
         
-        self.exampleView.shadowScale = CGFloat(sender.value * 2) + 1
+        self.exampleView.shadowScale = CGFloat(sender.value) + 1
     }
     
     @IBAction func shadowOpacity(_ sender: UISlider) {
         
         self.exampleView.shadowOpacity = sender.value
+        self.otherImage.shadowOpacity = sender.value
     }
     
     @IBAction func CHANGEiMAGE(_ sender: Any) {
         
         if self.imageviEW.image == image1{
             self.imageviEW.image = image2
+            self.otherImage.image = image2
         }else{
             self.imageviEW.image = image1
+            self.otherImage.image = image1
         }
         
-        self.perform(#selector(self.update), with: nil, afterDelay: 0.4)
-        
+        self.exampleView.updateShadow()
     }
     
     func update(){
@@ -59,6 +65,7 @@ class ViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        self.exampleView.updateShadow()
     }
 
     override func didReceiveMemoryWarning() {
